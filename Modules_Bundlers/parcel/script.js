74204320a3d7eb1025550b67f8fdcf4d02ede10c
KarlTal <talaverakarl04@gmail.com>
2023-08-25 22:35:42 +0100

    Finished parcel

---
+++ Modules_Bundlers/parcel/script.js
@@ -0,0 +1,7 @@
+import printUsername, { sally, Karl } from "./user.js";
+
+import { v4 } from "uuid";
+
+console.log(v4());
+printUsername(sally);
+printUsername(Karl);
---
+++ Modules_Bundlers/parcel/user.js
@@ -0,0 +1,11 @@
+export const sally = {
+  name: "Sally",
+};
+
+export const Karl = {
+  name: "Karl",
+};
+
+export default function printUsername(user) {
+  console.log(user.name);
+}
